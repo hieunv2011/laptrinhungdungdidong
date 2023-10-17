61d8dee9239f6e388e948cef2f58f56e5243c772
Hieu Nguyen <hieu.nv2001hust@gmail.com>
2023-10-17 17:35:11 +0700

    update

--- app/src/main/java/com/example/tuorguide/MainActivity.java
+++ app/src/main/java/com/example/tuorguide/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.tuorguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.app.Activity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtPassword=(EditText) findViewById(R.id.txtPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //alert
+                Toast.makeText(getApplicationContext(),"You press Login Button", Toast.LENGTH_LONG)
+            }
+        })
+
     }
 }
